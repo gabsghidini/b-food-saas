af791521be8c1fbf65119dc815b89c2f294ab6fa
Gabs Ghidini <gabsghidini@gmail.com>
2024-02-17 18:30:28 -0300

    FEAT: Updates on package.json and server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import AppDataSource from "./data-source";
 AppDataSource.initialize()
 	.then(() => {
 		console.log("Database conectada com sucesso! 🚀");
-		app.listen(3000, () => {
-			console.log("Servidor executando na porta 3000! 😎");
+		app.listen(666, () => {
+			console.log("Servidor executando na porta 666! 👿");
 		});
 	})
 	.catch((err) => {
